6c0e4fe0145de9d4e528aea81b157ecdc28fe64d
Thunus bastien <bastienthunus@gmail.com>
2023-04-04 11:41:02 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bck__image">
+  </div>
+  
+  <section class="component__container">
+    
+    <div>
+      <a href="https://www.figma.com/file/R6Cv9zzaK4q2S8b2SptcED/TFE?node-id=0%3A1&t=kPxfIydYeeTxbvc5-1" target="_blank">
+        <img src="./assets/Figma-logo.svg" class="logo" alt="Vite logo" />
+      </a>
+    </div>
+    
+    <HelloWorld msg="Thunus Bastien" />
+    
+  </section>
+
+</template>
+
+<style scoped lang="scss">
+
+.bck__image{
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  z-index: -99;
+  // background-image: url(./assets/Rectangle.jpg);
+  background: cover;
+}
+
+.component__container{
+  width: 100%;
+  place-items: center;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+  // background: rgba(255, 255, 255, 0.01);
+  // border: 1px solid rgba(255, 255, 255, 0.4);
+  // backdrop-filter: blur(17.5px);
+  // border-radius: 20px;
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+
+  <div class="content__container">
+    <div class="text__line">PromptPilot</div>
+  </div>
+
+  <h2>{{ msg }}</h2>
+
+</template>
+
+<style scoped lang="scss">
+
+  .content-container {
+  text-align: center;
+}
+
+.text__line {
+  font-variation-settings: "wght" 100, "ital" 0;
+  font-family: Oddval Variable;
+  font-size: 10vh;
+  line-height: 80%;
+  color: #ffffff;
+  white-space: nowrap;
+
+  animation-duration: 4s;
+  animation-iteration-count: infinite;
+  animation-name: change;
+  transition-timing-function: ease;
+
+  
+}
+
+@keyframes change {
+  0% {
+    font-variation-settings: "wght" 100, "ital" 0;
+  }
+
+  25% {
+    font-variation-settings: "wght" 900, "ital" 0;
+  }
+
+  50% {
+    font-variation-settings: "wght" 900, "ital" 1;
+  }
+
+  75% {
+    font-variation-settings: "wght" 100, "ital" 1;
+  }
+
+  100% {
+    font-variation-settings: "wght" 100, "ital" 0;
+  }
+}
+</style>
