73cb4292f4dbd793423c309539f1a8acb9e9c9a0
Thunus bastien <bastienthunus@gmail.com>
2023-04-10 14:53:18 +0200

    test modifier

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const props = defineProps(['id'])
 const Addparams = ref(null);
 const prompt = ref(null);
 const dialog = ref(null);
-const editedPrompt = ref(null);
+const editedPrompt = ref('');
+const selectedParams = ref({});
 
 fetch('http://localhost:3000/prompts/' + props.id)
 .then(response => response.json())
@@ -14,8 +15,13 @@ fetch('http://localhost:3000/prompts/' + props.id)
 
 fetch('http://localhost:3000/AddParameters/')
 .then(response => response.json())
-.then(data => Addparams.value = data)
-.catch(err => console.log("error fetch params"));
+.then(data => {
+  Addparams.value = data;
+  // initialiser les paramètres sélectionnés avec la valeur par défaut
+  data.forEach(param => {
+    selectedParams.value[param.titleShort] = param.params[0];
+  });
+})
 
 console.log(Addparams);
 
@@ -27,6 +33,13 @@ function closeDialog() {
   dialog.value.close();
 }
 
+const modifiedPrompt = computed(() => {
+  let modified = prompt.value.original_prompt;
+  Object.values(selectedParams.value).forEach(param => {
+    modified = modified.replace(`{${param.title}}`, param.param);
+  });
+  return modified;
+});
 
 </script>
 
@@ -34,7 +47,7 @@ function closeDialog() {
 
 <div v-if="prompt">
     <p>L'id est {{ id }}</p>
-    <textarea v-model="editedPrompt">{{ prompt.original_prompt }}</textarea>
+    <div>{{ modifiedPrompt }}</div>
   </div>
 
   <button @click="openDialog">Modifier</button>
@@ -43,85 +56,14 @@ function closeDialog() {
     <h2>Modifier le prompt</h2>
     <form v-for="paramPrompt in Addparams" :key="paramPrompt.id">
       <label>{{ paramPrompt.titleShort }}</label>
-      <label  v-for="params in paramPrompt">
-            {{ params.id }}
-            <input type="radio" name="params.id " value="params.param"/>
-      </label>
+        <label v-for="param in paramPrompt.params" :key="param.id">
+        {{ param.param }}
+        <input type="radio" :name="paramPrompt.titleShort" :value="param" v-model="selectedParams[paramPrompt.titleShort]"/>
+    </label>
     </form>
     <div>
       <button @click="closeDialog">Fermer</button>
     </div>
   </dialog>
-    
-    <!-- <dialog ref="dialog">
-        <h2>Modifier le prompt</h2>
-        <div>
-            <label>Aspect Ratio</label>
-            <input type="radio" v-model="params.category" value="0"/>
-            <input type="radio" v-model="prompt.category" value="1"/>
-            <input type="radio" v-model="prompt.category" value="2"/>
-            <input type="radio" v-model="prompt.category" value="3"/>
-            <input type="radio" v-model="prompt.category" value="4"/>
-        </div>
-        <div>
-            <label>Style</label>
-            <input type="radio" v-model="prompt.style" value="0"/>
-            <input type="radio" v-model="prompt.style" value="1"/>
-            <input type="radio" v-model="prompt.style" value="2"/>
-            <input type="radio" v-model="prompt.style" value="3"/>
-            <input type="radio" v-model="prompt.style" value="4"/>
-        </div>
-        <div>
-            <label>Character</label>
-            <input type="radio" v-model="prompt.character" value="0"/>
-            <input type="radio" v-model="prompt.character" value="1"/>
-            <input type="radio" v-model="prompt.character" value="2"/>
-            <input type="radio" v-model="prompt.character" value="3"/>
-            <input type="radio" v-model="prompt.character" value="4"/>
-        </div>
-        <div>
-            <label>Character</label>
-            <input type="radio" v-model="prompt.character" value="0"/>
-            <input type="radio" v-model="prompt.character" value="1"/>
-            <input type="radio" v-model="prompt.character" value="2"/>
-            <input type="radio" v-model="prompt.character" value="3"/>
-            <input type="radio" v-model="prompt.character" value="4"/>
-        </div>
-        <div>
-            <label>Location</label>
-            <input type="radio" v-model="prompt.location" />
-        </div>
-        <div>
-            <label>Action</label>
-            <input type="radio" v-model="prompt.action" />
-        </div>
-        <div>
-            <label>Color</label>
-            <input type="radio" v-model="prompt.color" />
-        </div>
-        <div>
-            <label>Artist</label>
-            <input type="radio" v-model="prompt.artist" />
-        </div>
-        <div>
-            <label>Background</label>
-            <input type="radio" v-model="prompt.background" />
-        </div>
-        <div>
-            <label>Resolution</label>
-            <input type="radio" v-model="prompt.resolution" />
-        </div>
-        <div>
-            <label>Renderer</label>
-            <input type="radio" v-model="prompt.renderer" />
-        </div>
-        <div>
-            <label>Lighting</label>
-            <input type="radio" v-model="prompt.lighting" />
-        </div>
-        <div>
-            <button @click="dialog.value.close()">Fermer</button>
-        </div>
-    </dialog> -->
 
 </template>
