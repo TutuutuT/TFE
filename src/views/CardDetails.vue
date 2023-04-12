9f7be1c3489ffa7f28e8c6a6f71b1adb7b776b60
Bastien Thunus <95473565+TutuutuT@users.noreply.github.com>
2023-04-12 15:56:10 +0200

    bouton switch etat checked ok

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -26,8 +26,6 @@ function closeDialog() {
   dialog.value.close();
 }
 
-computed
-
 </script>
 
 <template>
@@ -40,23 +38,30 @@ computed
 
   <button @click="openDialog">Modifier</button>
 
-  <dialog ref="dialog">
+  <dialog ref="dialog" class=" p-1 space-x-1 bg-white/1 backdrop-blur-sm rounded-xl text-white border border-white/40 p-8">
     <h2>Modifier le prompt</h2>
-    <form v-for="paramPrompt in Addparams" :key="paramPrompt.id">
+    <form class="h-8 my-2 rounded w-96 flex" v-for="paramPrompt in Addparams" :key="paramPrompt.id">
       
-      <label class="label__title">{{ paramPrompt.titleShort }}</label>
-      <label class="label__param"> None
-      <input type="radio" :name="paramPrompt.titleShort" :value="null" v-model="selectedParams[paramPrompt.id]"/>
-      </label>
+      <label class="label__title ml-2 mr-4 w-10">{{ paramPrompt.titleShort }}</label>
 
-        <label class="label__param" v-for="param in paramPrompt.params" :key="param.id"> {{ param.param }}
+      <div class=" flex justify-between w-full bg-zinc-800 rounded-full">
 
-        <input type="radio" :name="paramPrompt.titleShort" :value="param.param" v-model="selectedParams[paramPrompt.id]"/>
+        <label class="label__param h-8 w-14 py-[3px] text-center text-sm leading-6 text-white hover:bg-zinc-500/30 rounded-full shadow"> None
 
-    </label>
+          <input class="sr-only" type="radio" :name="paramPrompt.titleShort" :value="null" v-model="selectedParams[paramPrompt.id]"/>
 
-    
-  </form>
+        </label>
+
+  
+        <label class="label__param w-14 py-[3px] text-center label__param h-8 text-sm leading-6 text-white hover:hover:bg-zinc-500/30 rounded-full shadow transition-all" v-for="param in paramPrompt.params" :key="param.id"> {{ param.param }}
+  
+          <input class="sr-only" type="radio" :name="paramPrompt.titleShort" :value="param.param" v-model="selectedParams[paramPrompt.id]"/>
+  
+        </label>
+
+      </div>
+
+    </form>
     <div>
       <button @click="closeDialog">Fermer</button>
     </div>
@@ -66,15 +71,8 @@ computed
 
 <style lang="scss">
 
-.label{
-  &__title{
-    margin-right: 30px;
-  }
-
-  &__param{
-    inline-size: 300px;
-  }
-  
+label:has(input[type="radio"]:checked) {
+  @apply bg-zinc-500;
 }
 
 </style>
