1a113b28ad6feccb439a6e1156b9409856c50b81
Thunus bastien <bastienthunus@gmail.com>
2023-04-28 12:30:08 +0200

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,10 +6,10 @@ import { gsap } from 'gsap';
 
 <template>
 
-<footer class="mb-6 mt-48 mx-44 lg:mx-auto h-[60svh]">
-    <div class="flex flex-col lg:flex-row lg:mx-[16vw] justify-between text-lg">
-        <p class="lg:mx-12">L'éditeur de prompt pour Midjourney</p>
-        <ul class="italic flex flex-col justify-between lg:mx-12 lg:px-0 mx-auto">
+<footer class="mb-6 mt-48 h-[40svh] font-semibold">
+    <p class="w-full mb-20 text-xl lg:mx-20 mx-10">L'éditeur de prompt pour Midjourney</p>
+    <div class="text-lg">
+        <ul class="italic flex flex-col justify-between lg:mx-20 mx-10 leading-10">
             <li>ezaeaze</li>
             <li>Thunus Bastien</li>
             <li>DWT</li>
